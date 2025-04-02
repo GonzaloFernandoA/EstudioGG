7238af06eb77a7ec0bc98a19e9fbd1e68c28ae76
GonzaloFernandoA <gonzalofernandoa@gmail.com>
2025-04-02 19:23:56 -0300

    add datatables.js

--- src/main/java/com/whatsup/bot/controler/BotController.java
+++ src/main/java/com/whatsup/bot/controler/BotController.java
@@ -6,8 +6,10 @@ import com.whatsup.bot.service.WhatsAppService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BotController {
     
+    @Value("${build.time}")
+    private String buildTime;
+    
     @Autowired
     tokenService token;
     
     @Autowired
     WhatsAppService whatsAppService;
     private Logger log = LoggerFactory.getLogger(BotController.class);
-
+    
     @GetMapping("/hello")
     public ResponseEntity<String> exportOpportunity() {
         return new ResponseEntity<>("Hello", HttpStatus.OK);
@@ -55,8 +60,10 @@ public class BotController {
     }
     
     @GetMapping("/index")
-    public String home() {
+    public String index(Model model) {
+        
+        log.info(buildTime);
+        model.addAttribute("buildTime", buildTime);
         return "index";
-
     }
 }
